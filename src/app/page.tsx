84dc9d21d5456c72a232cb35b8baec28bd611a91
Christophe anfry <66621302+Canfry@users.noreply.github.com>
2025-02-28 18:03:39 +0100

    Change index page text to check deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>User Posts</h1>
+      <h1 className="text-2xl font-bold">List of my posts</h1>
       {posts.map((post) => (
         <p key={post.id}>{post.title}</p>
       ))}
