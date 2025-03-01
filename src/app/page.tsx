b96fe21933ea735899bd777bb617c2fbb87d23b8
Christophe anfry <66621302+Canfry@users.noreply.github.com>
2025-03-01 10:00:54 +0100

    Add style to clerk buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import { getPosts } from "@/db/postQueries";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+// import { PostsRecord } from "@/xata";
 import { auth, currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
+  const { userId } = await auth();
+  console.log(userId);
   // const post = await createPost({
   //   title: "My second post",
   //   body: "This is my second post",
+  //   userId: userId
   // } as PostsRecord);
 
-  const authInfo = await auth();
-  console.log(authInfo);
+
 
   const user = await currentUser();
   console.log(user?.emailAddresses[0].emailAddress);
@@ -17,10 +21,15 @@ export default async function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-2xl font-bold">List of my posts</h1>
-      {posts.map((post) => (
-        <p key={post.id}>{post.title}</p>
-      ))}
+      {/* <h1 className="text-2xl font-bold">List of my posts</h1> */}
+      <SignedOut>
+        <h1 className="text-2xl font-bold">Please sign in to see the posts</h1>
+      </SignedOut>
+      <SignedIn>
+        {posts.map((post) => (
+          <p key={post.id}>{post.title}</p>
+        ))}
+      </SignedIn>
     </div>
   );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -16,8 +16,16 @@ export default function Nav() {
                 <Link href="/" className="text-2xl font-bold">User Posts</Link>
                 <div className="flex items-center gap-4">
                     <SignedOut>
-                        <SignInButton />
-                        <SignUpButton />
+                        <SignInButton mode="modal">
+                            <button className="px-4 py-2 rounded-md bg-gray-100 hover:bg-gray-300 transition-colors cursor-pointer text-black">
+                                Sign In
+                            </button>
+                        </SignInButton>
+                        <SignUpButton mode="modal">
+                            <button className="px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 transition-colors cursor-pointer">
+                                Sign Up
+                            </button>
+                        </SignUpButton>
                     </SignedOut>
                     <SignedIn>
                         <UserButton />
