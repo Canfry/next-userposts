0a033c5c49ae103ac7c0eb2c8c26835e2b18278e
Christophe anfry <66621302+Canfry@users.noreply.github.com>
2025-03-10 09:27:36 +0100

    Add slice to post body over 100 chars

--- src/components/PostCards.tsx
+++ src/components/PostCards.tsx
@@ -4,7 +4,7 @@ export default function PostCards({ post }: { post: PostsRecord }) {
     return (
         <>
             <h1 className="text-2xl font-bold">{post.title}</h1>
-            <p className="text-gray-500">{post.body}</p>
+            <p className="text-gray-500">{post.body.length > 100 ? post.body.slice(0, 100) + "..." : post.body}</p>
         </>
     );
 }
